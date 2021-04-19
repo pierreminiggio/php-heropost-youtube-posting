5f4b3f36b4a47ca019307289403f78650cf87d9e
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-04-19 21:25:26 +0200

    executor

--- src/JSExecutor.php
+++ src/JSExecutor.php
@@ -8,4 +8,41 @@ namespace PierreMiniggio\HeropostYoutubePosting;
 class JSExecutor
 {
 
+    public function __construct(private string $nodePath = 'node')
+    {
+    }
+
+    public function execute(
+        string $heropostLogin,
+        string $herpostPassword,
+        string $youtubeChannelId,
+        string $title,
+        string $description,
+        int $youtubeCategoryId,
+        string $videoFilePath
+    ): ?string
+    {
+        return shell_exec(
+            $this->nodePath
+            . ' '
+            . __DIR__
+            . DIRECTORY_SEPARATOR
+            . '..'
+            . DIRECTORY_SEPARATOR
+            . 'post.js '
+            . escapeshellarg($heropostLogin)
+            . ' '
+            . escapeshellarg($herpostPassword)
+            . ' '
+            . escapeshellarg($youtubeChannelId)
+            . ' '
+            . escapeshellarg($title)
+            . ' '
+            . escapeshellarg($description)
+            . ' '
+            . $youtubeCategoryId
+            . ' '
+            . escapeshellarg($videoFilePath)
+        );
+    }
 }
---
+++ test-executor.php
@@ -0,0 +1,12 @@
+<?php
+
+use PierreMiniggio\HeropostYoutubePosting\JSExecutor;
+
+require __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+/** @var string[] $args */
+$args = $argv;
+array_shift($args);
+
+$executor = new JSExecutor();
+echo $executor->execute(...$args);
